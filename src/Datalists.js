920ce6ab7bbddf7370ace514f7fbb4dcbcd0c797
queuing4oranges <katjazenker@gmail.com>
2024-08-08 22:03:37 +0200

    Add Datalist

---
+++ src/Datalists.js
@@ -0,0 +1,91 @@
+export const wherefromPlaces = [
+	{
+		id: 1,
+		place: 'Bowling s Lesbotočem',
+	},
+	{
+		id: 2,
+		place: 'Knížní Klub',
+	},
+	{
+		id: 3,
+		place: 'Lesbotoč MUSIC KVÍZ',
+	},
+	{
+		id: 4,
+		place: 'Prague Pride',
+	},
+	{
+		id: 5,
+		place: 'Speed Dating',
+	},
+];
+
+export const ageGroups = [
+	{
+		id: 1,
+		age: '20-25',
+	},
+	{
+		id: 2,
+		age: '26-30',
+	},
+	{
+		id: 3,
+		age: '31-35',
+	},
+	{
+		id: 4,
+		age: '36-40',
+	},
+	{
+		id: 5,
+		age: '41-45',
+	},
+	{
+		id: 6,
+		age: '46-50',
+	},
+	{
+		id: 7,
+		age: '50+',
+	},
+];
+
+export const eventTypes = [
+	{
+		id: 1,
+		type: 'Lesbotoc Event'
+	},
+	{
+		id: 2,
+		type: 'Speed Dating'
+	},
+	{
+		id: 3,
+		type: 'Other Event'
+	},
+	{
+		id: 4,
+		type: 'Lesbotoc Camp'
+	}
+]
+
+export const navLinks = [
+	{
+		to: '/contacts',
+		name: 'Contacts'
+	},
+	{
+		to: '/events',
+		name: 'Events'
+	},
+	{
+		to: '/pictures',
+		name: 'Pictures'
+	},
+	{
+		to: '/more',
+		name: 'More'
+	}
+]
